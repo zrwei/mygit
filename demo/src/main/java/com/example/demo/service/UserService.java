53ce317867a0a41a8977be9b874ce5a4fd032e16
zrwei <40288490+zrwei@users.noreply.github.com>
2025-04-27 11:33:56 +0800

    Delete demo directory

--- demo/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.User;
-
-public interface UserService {
-    User findUserByUsernameAndPassword(String username, String password);
-}    
